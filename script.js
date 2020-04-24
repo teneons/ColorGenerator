34847c4dcc3c96ad9f651e14005730984c79301d
teneons <teneons@gmail.com>
2020-04-24 12:53:58 +0300

    Ver. 1.2. Add modal win. 'Copier'. Change cursors

--- script.js
+++ script.js
@@ -9,16 +9,32 @@ const generateColor = () => {
     //apply to html
     document.body.style.backgroundColor = `#${color}`;
     //out color
-    outColor.value = `#${color}`
+    outColor.value = `#${color}`;
 
 }
 
 document.getElementById('btnG').addEventListener('click', generateColor)
 
 
+//Copied modal window
+
+function showCopy() { document.getElementById('modalCopied').style.visibility = 'visible'; }
+
+function hideCopy() { document.getElementById('modalCopied').style.visibility = 'hidden'; }
+
+
 //copy color
 outColor.addEventListener('click', () => {
     outColor.select();
     document.execCommand('copy');
+
+    setTimeout(() => {
+        showCopy()
+    }, 50)
+
+    setTimeout(() => {
+        hideCopy()
+    }, 300)
 })
+
 
